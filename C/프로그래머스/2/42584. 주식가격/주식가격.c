6e6b861d86e94a45bb2bcacd7c8132e34a7a7d4a
Snowyn13 <devsnowyn@gmail.com>
2026-04-02 00:39:26 +0900

    [level 2] Title: 주식가격, Time: 26.86 ms, Memory: 27.5 MB -BaekjoonHub

---
+++ C/프로그래머스/2/42584. 주식가격/주식가격.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef struct{
+    int* data;
+    int top;
+    int capacity;
+}Stack;
+
+void init(Stack* s,int capacity){
+    s->top=-1;
+    s->data=(int*)malloc(sizeof(int)*capacity);
+    s->capacity=capacity;
+}
+
+int empty(Stack* s){
+    return s->top==-1;
+}
+
+int is_full(Stack* s){
+    return s->top==s->capacity-1;
+}
+
+int push(Stack* s,int val){
+    if(!is_full(s))
+        return s->data[++(s->top)]=val;
+    else
+        return -1;
+}
+
+int pop(Stack*s){
+    if(!empty(s))
+        return s->data[(s->top)--];
+    else
+        return -1;
+}
+
+int peek(Stack* s){
+    if(!empty(s))
+        return s->data[s->top];
+    else
+        return -1;
+}
+
+int* solution(int prices[], size_t prices_len) {
+    int* answer = (int*)malloc(sizeof(int)*prices_len);
+    Stack s;
+    init(&s,prices_len);
+    
+    for(int i=0;i<prices_len;i++){
+        while(!empty(&s)&&prices[i]<prices[peek(&s)]){
+            int topn=pop(&s);
+            answer[topn]=i-topn;
+        }
+        push(&s,i);
+    }
+    while(!empty(&s)){
+        int topn = pop(&s);
+        answer[topn] = prices_len - 1 - topn;
+    }
+    free(s.data);
+    return answer;
+}
