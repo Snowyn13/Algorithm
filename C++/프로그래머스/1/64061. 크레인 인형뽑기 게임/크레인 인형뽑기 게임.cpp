9d4d10c79ca886e662cd8d7d8bd7b2e86951c61b
Snowyn13 <devsnowyn@gmail.com>
2026-04-14 18:14:30 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- C++/프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ C++/프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -1,101 +1,70 @@
 #include <string>
 #include <vector>
-
 using namespace std;
 
 typedef struct{
-    int * data;
+    int* data;
     int top;
     int capacity;
-}Struct;
+}Stack;
 
-void init(Struct* s,int capacity){
-    s->top=-1;
+void init(Stack* s,int capacity){
     s->data=(int*)malloc(sizeof(int)*capacity);
+    s->top=-1;
     s->capacity=capacity;
 }
 
-int empty(Struct* s){
+int empty(Stack* s){
     return s->top==-1;
 }
 
-int is_full(Struct* s){
+int is_full(Stack* s){
     return s->top==s->capacity-1;
 }
 
-int push(Struct* s,int val){
+void push(Stack* s,int val){
     if(!is_full(s))
-        return s->data[++(s->top)]=val;
-    return -1;
+        s->data[++(s->top)]=val;
 }
 
-int pop(Struct* s){
+int pop(Stack* s){
     if(!empty(s))
         return s->data[(s->top)--];
     return -1;
 }
 
-int peek(Struct* s){
+int peek(Stack* s){
     if(!empty(s))
         return s->data[s->top];
     return -1;
 }
 
-void free_stack(Struct* s){
-    free(s->data);
-}
-
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
     int n = board.size();
-    int m = moves.size();
 
-    // board를 C식 2차원 배열로 복사
-    int** arr = (int**)malloc(sizeof(int*) * n);
-    for(int i = 0; i < n; i++){
-        arr[i] = (int*)malloc(sizeof(int) * n);
-        for(int j = 0; j < n; j++){
-            arr[i][j] = board[i][j];
-        }
-    }
+    Stack s;
+    init(&s, n*n);
 
-    // moves를 C식 배열로 복사
-    int* moveArr = (int*)malloc(sizeof(int) * m);
-    for(int i = 0; i < m; i++){
-        moveArr[i] = moves[i];
-    }
+    for(int i=0;i<moves.size();i++){
+        int col = moves[i]-1;
 
-    Struct s;
-    init(&s, n * n);
+        for(int row=0;row<n;row++){
+            if(board[row][col]!=0){
+                int doll = board[row][col];
+                board[row][col]=0;
 
-    for(int i = 0; i < m; i++){
-        int col = moveArr[i] - 1;
-
-        for(int row = 0; row < n; row++){
-            if(arr[row][col] != 0){
-                int doll = arr[row][col];
-                arr[row][col] = 0;
-
-                if(!empty(&s) && peek(&s) == doll){
+                if(!empty(&s) && peek(&s)==doll){
                     pop(&s);
-                    answer += 2;
+                    answer+=2;
+                }else{
+                    push(&s,doll);
                 }
-                else{
-                    push(&s, doll);
-                }
-
                 break;
             }
         }
     }
 
-    free_stack(&s);
-
-    for(int i = 0; i < n; i++){
-        free(arr[i]);
-    }
-    free(arr);
-    free(moveArr);
-
+    free(s.data);
     return answer;
 }
