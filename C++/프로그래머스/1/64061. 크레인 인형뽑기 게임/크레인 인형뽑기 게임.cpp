63c342ea552968fe029eac0dc021bab00456eaad
Snowyn13 <devsnowyn@gmail.com>
2026-04-15 17:59:22 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.03 ms, Memory: 4.02 MB -BaekjoonHub

--- C++/프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ C++/프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -2,69 +2,83 @@
 #include <vector>
 using namespace std;
 
-typedef struct{
+typedef struct stack {
     int* data;
     int top;
     int capacity;
 }Stack;
 
-void init(Stack* s,int capacity){
-    s->data=(int*)malloc(sizeof(int)*capacity);
-    s->top=-1;
-    s->capacity=capacity;
+void free_s(Stack* s) {
+    free(s->data);
 }
 
-int empty(Stack* s){
-    return s->top==-1;
+void init(Stack* s, int capacity) {
+    s->capacity = capacity;
+    s->top = -1;
+    s->data = (int*)malloc(sizeof(int) * s->capacity);
 }
 
-int is_full(Stack* s){
-    return s->top==s->capacity-1;
+int empty_s(Stack* s) {
+    return s->top == -1;
 }
 
-void push(Stack* s,int val){
-    if(!is_full(s))
+int full_s(Stack* s) {
+    return s->top == s->capacity - 1;
+}
+
+void push(Stack* s, int val) {
+    if (full_s(s)) {
+        fprintf(stderr, "스택 포화 에러 \n");
+        return;
+    }
+    else
         s->data[++(s->top)]=val;
 }
 
-int pop(Stack* s){
-    if(!empty(s))
+int pop(Stack* s) {
+    if (empty_s(s)) {
+        fprintf(stderr, "스택 공백 에러 \n");
+        return -1;
+    }
+    else
         return s->data[(s->top)--];
-    return -1;
 }
 
-int peek(Stack* s){
-    if(!empty(s))
+int peek(Stack* s) {
+    if (empty_s(s)) {
+        fprintf(stderr, "스택 공백 에러 \n");
+        return -1;
+    }
+    else
         return s->data[s->top];
-    return -1;
 }
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
+
     int n = board.size();
-
     Stack s;
-    init(&s, n*n);
+    init(&s, n * n);
 
-    for(int i=0;i<moves.size();i++){
-        int col = moves[i]-1;
-
-        for(int row=0;row<n;row++){
-            if(board[row][col]!=0){
+    for (int i = 0; i < moves.size(); i++) {
+        int col = moves[i] - 1;
+        for (int row = 0; row < n; row++) {
+            if (board[row][col] != 0) {
                 int doll = board[row][col];
-                board[row][col]=0;
+                board[row][col] = 0;
 
-                if(!empty(&s) && peek(&s)==doll){
+                if (!empty_s(&s) && peek(&s) == doll) {
                     pop(&s);
-                    answer+=2;
-                }else{
-                    push(&s,doll);
+                    answer += 2;
+                }
+                else {
+                    push(&s, doll);
                 }
                 break;
             }
         }
     }
 
-    free(s.data);
+    free_s(&s);
     return answer;
 }
