09807cac574e5ebe19d351e0614a349cea39f2e0
Snowyn13 <devsnowyn@gmail.com>
2026-04-15 13:32:43 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- C++/프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ C++/프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -2,73 +2,92 @@
 #include <vector>
 using namespace std;
 
-typedef struct{
+typedef struct queue{
     int* data;
     int front;
     int rear;
     int capacity;
 }Queue;
 
-void init(Queue* q,int capacity){
-    q->capacity = capacity + 1;
-    q->data = (int*)malloc(sizeof(int)*q->capacity);
-    q->front = 0;
-    q->rear = 0;
+void init(Queue* q, int capacity)
+{
+    q->capacity=capacity+1;
+    q->data=(int*)malloc(sizeof(int)*q->capacity);
+    q->front=0;
+    q->rear=0;
 }
 
-int empty_q(Queue* q){
-    return q->front == q->rear;
+int empty_q(Queue* q)
+{
+    return q->front==q->rear;
 }
 
-int full_q(Queue* q){
-    return (q->rear + 1) % q->capacity == q->front;
+int full_q(Queue* q)
+{
+    return (q->rear+1)%q->capacity==q->front;
 }
 
-void enqueue(Queue* q,int val){
-    if(!full_q(q)){
-        q->rear = (q->rear + 1) % q->capacity;
-        q->data[q->rear] = val;
+void enqueue(Queue* q, int val)
+{
+    if(full_q(q)){
+        fprintf(stderr,"큐 포화 에러\n");
+        return;
     }
-    else return;
+    else{
+        q->rear=(q->rear+1)%q->capacity;
+        q->data[q->rear]=val;
+    }
 }
 
-int dequeue(Queue* q){
-    if(!empty_q(q)){
-        q->front = (q->front + 1) % q->capacity;
+int dequeue(Queue* q)
+{
+    if(empty_q(q)){
+        fprintf(stderr,"큐 공백 에러\n");
+        return -1;
+    }
+    else{
+        q->front=(q->front+1)%q->capacity;
         return q->data[q->front];
     }
-    return -1;
 }
 
-int peek(Queue* q){
-    if(!empty_q(q))
+int peek(Queue* q)
+{
+    if(empty_q(q)){
+        fprintf(stderr,"큐 공백 에러 \n");
+        return -1;
+    }
+    else{
         return q->data[(q->front+1)%q->capacity];
-    return -1;
+    }
+}
+
+void free_q(Queue* q)
+{
+    free(q->data);
 }
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     Queue q;
     init(&q, progresses.size());
-
+    
     for(int i=0;i<progresses.size();i++){
-        int remain = 100 - progresses[i];
-        int day = remain / speeds[i];
-        if(remain % speeds[i] != 0) day++;
+        int remain=100-progresses[i];
+        int day=remain/speeds[i];
+        if(remain%speeds[i]!=0)
+            day++;
         enqueue(&q, day);
     }
-
     while(!empty_q(&q)){
-        int count = 1;
-        int cur = dequeue(&q);
-
-        while(!empty_q(&q) && peek(&q) <= cur){
+        int a=dequeue(&q);
+        int c=1;
+        while(!empty_q(&q)&&peek(&q)<=a){
             dequeue(&q);
-            count++;
+            c++;
         }
-        answer.push_back(count);
+        answer.push_back(c);
     }
-
-    free(q.data);
+    free_q(&q);
     return answer;
 }
