f89d298d2d4cac131968e326d14ec805b307840e
Snowyn13 <devsnowyn@gmail.com>
2026-04-20 21:16:38 +0900

    [level 2] Title: 짝지어 제거하기, Time: 4.91 ms, Memory: 6.26 MB -BaekjoonHub

--- C++/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ C++/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -1,76 +1,86 @@
 #include <iostream>
-#include <cstdlib>
-#include <string>
-
+#include<string>
 using namespace std;
 
-typedef struct{
-    char * data;
+typedef struct stack
+{
+    char* data;
     int top;
     int capacity;
-} Stack;
+}Stack;
 
-void init(Stack* s,int capacity){
-    s->data = (char*)malloc(sizeof(char)*capacity);
-    s->top = -1;
-    s->capacity = capacity;
-}
-
-void free_stack(Stack* s){
+void free_s(Stack* s)
+{
     free(s->data);
 }
 
-int empty(Stack* s){
-    return s->top == -1;
+void init(Stack* s, int capacity)
+{
+    s->capacity=capacity;
+    s->data=(char*)malloc(sizeof(char)*s->capacity);
+    s->top=-1;
 }
 
-int is_full(Stack* s){
-    return s->top == s->capacity - 1;
+int empty(Stack* s)
+{
+    return s->top==-1;
 }
 
-void push(Stack* s,char val){
-    if(!is_full(s))
-        s->data[++(s->top)] = val;
+int full(Stack* s)
+{
+    return s->top==s->capacity-1;
 }
 
-char pop(Stack* s){
-    if(!empty(s))
+void push(Stack* s,char val)
+{
+    if(full(s))
+    {
+        fprintf(stderr,"스택 포화 에러\n");
+        return;
+    }
+    else
+        s->data[++(s->top)]=val;
+}
+
+char pop(Stack* s)
+{
+    if(empty(s))
+    {
+        fprintf(stderr,"스택 공백 에러\n");
+        return '\0';
+    }
+    else
         return s->data[(s->top)--];
-    else
-        return '\0';
 }
 
-char peek(Stack* s){
-    if(!empty(s))
-        return s->data[s->top];
-    else
+char peek(Stack* s)
+{
+    if(empty(s))
+    {
+        fprintf(stderr,"스택 공백 에러\n");
         return '\0';
+    }
+    else
+        return s->data[(s->top)];
 }
 
 int solution(string s)
 {
-    int len = s.length();
-
-    if(len % 2 != 0)
-        return 0;
-
-    Stack stack;
-    init(&stack, len);
+    int answer = -1;
+    int len=s.length();
     
-    for(int i = 0; i < len; i++){
-        char current = s[i];
-        
-        if(!empty(&stack) && peek(&stack) == current){
-            pop(&stack);
-        }
-        else{
-            push(&stack, current);
-        }
+    Stack st;
+    init(&st,len);
+    
+    for(int i=0;i<len;i++)
+    {
+        char c=s[i];
+        if(!empty(&st) && peek(&st)==c)
+            pop(&st);
+        else
+            push(&st,c);
     }
-    
-    int answer = empty(&stack) ? 1 : 0;
-
-    free_stack(&stack);
-
+    answer=empty(&st)?1:0;
+    free_s(&st);
     return answer;
 }
