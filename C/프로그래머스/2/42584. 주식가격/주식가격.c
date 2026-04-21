9e58a0ec352229870bb0ec757c537b2fb5d45464
Snowyn13 <devsnowyn@gmail.com>
2026-04-21 16:31:19 +0900

    [level 2] Title: 주식가격, Time: 26.26 ms, Memory: 27.5 MB -BaekjoonHub

--- C/프로그래머스/2/42584. 주식가격/주식가격.c
+++ C/프로그래머스/2/42584. 주식가격/주식가격.c
@@ -66,9 +66,10 @@ int peek(Stack* s)
 // prices_len은 배열 prices의 길이입니다.
 int* solution(int prices[], size_t prices_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int)*prices_len);
     Stack s;
     init(&s,prices_len);
-    int* answer = (int*)malloc(sizeof(int)*prices_len);
+    
     for(int i=0;i<prices_len;i++)
     {
         while(!empty(&s)&&prices[i]<prices[peek(&s)])
@@ -78,6 +79,7 @@ int* solution(int prices[], size_t prices_len) {
         }
         push(&s,i);
     }
+    
     while(!empty(&s))
     {
         int topn=pop(&s);
