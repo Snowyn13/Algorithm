74df9fe066874da3ad01136172eb95d36fde2d95
Snowyn13 <devsnowyn@gmail.com>
2026-05-04 00:39:52 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 1969.68 ms, Memory: 17.2 MB -BaekjoonHub

--- C/프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.c
+++ C/프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.c
@@ -58,13 +58,11 @@ void distribute(Member* member, int money) {
     }
 }
 
-// enroll_len은 배열 enroll의 길이입니다.
-// referral_len은 배열 referral의 길이입니다.
-// seller_len은 배열 seller의 길이입니다.
-// amount_len은 배열 amount의 길이입니다.
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
-int* solution(const char* enroll[], size_t enroll_len, const char* referral[], size_t referral_len, const char* seller[], size_t seller_len, int amount[], size_t amount_len) {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+int* solution(const char* enroll[], size_t enroll_len,
+              const char* referral[], size_t referral_len,
+              const char* seller[], size_t seller_len,
+              int amount[], size_t amount_len) {
+    
     Member* members = (Member*)malloc(sizeof(Member) * enroll_len);
 
     for (int i = 0; i < enroll_len; i++) {
@@ -90,6 +88,8 @@ int* solution(const char* enroll[], size_t enroll_len, const char* referral[], s
     for (int i = 0; i < enroll_len; i++) {
         answer[i] = members[i].profit;
     }
+
+    free(members);
 
     return answer;
 }
